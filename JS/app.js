62af2ef963f1271ccf88e5b22b35de5754c7f42e
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-10-08 14:16:40 -0500

    filtered data by selecting dropdown and search bar

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,82 @@
-alert("ciao bella me piace parllare italiano");
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+const baseURL = `https://jsonplaceholder.typicode.com/users`;
+
+const dataSet = [];
+
+fetch(baseURL)
+  .then((blob) => blob.json())
+  .then((data) => dataSet.push(...data));
+
+const findMatches = (wordToMatch, dataSet) => {
+  return dataSet.filter((data) => {
+    //regex
+    const regex = new RegExp(wordToMatch, "gi");
+
+    return data.name.match(regex) || data.email.match(regex);
+  });
+};
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, dataSet);
+
+  const html = matchArray
+    .map((data) => {
+      return `
+      <li>
+      <span class="name">${data.name}</span>
+      <span class="email">${data.email}</span>
+      </li>
+      `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+
+const errorMSG = document.querySelector("#error");
+
+selectDropdown();
+
+function selectDropdown() {
+  const cardType = document.getElementById("cardType");
+  const selectedValue = cardType.options[cardType.selectedIndex].value;
+
+  if (selectedValue == "selectCard") {
+    // alert("selectCard");
+
+    const html2 = dataSet
+      .map((data) => {
+        return `
+        <li>
+        <span class="name">${data.name}</span>
+        <span class="email">${data.email}</span>
+        </li>
+        `;
+      })
+      .join("");
+    suggestions.innerHTML = html2;
+  } else if (selectedValue == "email") {
+    const html3 = dataSet
+      .map((data) => {
+        return `
+      <li>
+      <span class="email">${data.email}</span>
+      </li>
+      `;
+      })
+      .join("");
+    suggestions.innerHTML = html3;
+  } else if (selectedValue == "name") {
+    const html4 = dataSet
+      .map((data) => {
+        return `
+      <li>
+      <span class="email">${data.name}</span>
+      </li>
+      `;
+      })
+      .join("");
+    suggestions.innerHTML = html4;
+  }
+}
+searchInput.addEventListener("keyup", displayMatches);
